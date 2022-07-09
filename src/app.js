70cf6b5f930012939c16eb8e8d7895b3bf161554
kneerace <kneerace@gmail.com>
2022-07-09 11:53:49 -0400

    orgarnized express code and all working as expectd

--- src/app.js
+++ src/app.js
@@ -1,17 +1,32 @@
+// const { json, application } = require("express");
 const express = require("express");
 const app = express();
+const pastes = require("./data/pastes-data");
+app.use(express.json());
 
-// TODO: Follow instructions in the checkpoint to implement ths API.
+
+const pastesRouter = require("./pastes/pastes.router");
+
+
+app.use("/pastes", pastesRouter); // its app.use not app.get, as get is defined in the router
+
+//POST handler
+app.post("/pastes", pastesRouter);
+
 
 // Not found handler
 app.use((request, response, next) => {
-  next(`Not found: ${request.originalUrl}`);
+  next({
+    status:404, 
+    message :`Not found: ${request.originalUrl}`
+})
 });
 
 // Error handler
 app.use((error, request, response, next) => {
   console.error(error);
-  response.send(error);
+  const {status = 500, message = "Something went wrong!!" } = error;
+  response.status(status).json({error: message});
 });
 
 module.exports = app;
